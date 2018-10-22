26a5d523e699d8bb912fd21256e80221dc43d84b
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-10-22 14:18:42 +0300

    add resource tests

--- test/api/resource.js
+++ test/api/resource.js
@@ -3,19 +3,19 @@ const moxios = require('moxios');
 
 const {Transport, Resource, Resources} = require('../../src');
 
-describe('Resource', function () {
+describe('Resource', () => {
   let transport;
-  beforeEach(function () {
+  beforeEach(() => {
     // import and pass your custom axios instance to this method
     moxios.install();
     transport = new Transport();
   });
 
-  afterEach(function () {
+  afterEach(() => {
     // import and pass your custom axios instance to this method
     moxios.uninstall();
   });
-  it('create new resource', function () {
+  it('create new resource', () => {
     const resourceJson = {_id: '123', name: 'X', __v: 0};
     const resources = new Resources(transport);
     const res = resources.create();
@@ -25,7 +25,7 @@ describe('Resource', function () {
     assert.equal(res.id, '123');
     assert.equal(res.isDirty(), true);
   });
-  it('resources update', function () {
+  it('resources update', () => {
     const resources = new Resources(transport);
     return resources.update()
       .reflect()
@@ -33,7 +33,7 @@ describe('Resource', function () {
         assert.equal(promise.isRejected(), true);
       });
   });
-  it('constructing with valid json', function () {
+  it('constructing with valid json', () => {
     const resourceJson = {_id: '123', name: 'X', __v: 0};
     const res = new Resource(transport, resourceJson);
     assert.equal(res.name(), 'X');
@@ -44,26 +44,67 @@ describe('Resource', function () {
     assert.deepEqual(res.getChanges(), {});
     assert.deepEqual(res.version, 0);
   });
-  it('constructing with invalid json', function () {
+  it('constructing with invalid json', () => {
     const resourceJson = 'invalid';
     assert.throws(() => new Resource(transport, resourceJson), Error);
   });
-  it('isDirty if change happen', function () {
-    const resourceJson = {_id: '123', name: 'X', __v: 0};
-    const res = new Resource(transport, resourceJson);
-    res.name('Y');
-    assert.deepEqual(res.getChanges(), {name: 'Y'});
-    assert.equal(res.isDirty(), true);
+  describe('modify', () => {
+    it('isDirty if change happen', () => {
+      const resourceJson = {_id: '123', name: 'X', __v: 0};
+      const res = new Resource(transport, resourceJson);
+      res.name('Y');
+      assert.deepEqual(res.getChanges(), {name: 'Y'});
+      assert.equal(res.isDirty(), true);
+    });
+    it('possible to change multiple', () => {
+      const resourceJson = {_id: '123', name: 'X', __v: 0};
+      const res = new Resource(transport, resourceJson);
+      assert.equal(res.name('Y').location.site('oulu').name(), 'Y');
+      assert.deepEqual(res.getChanges(), {name: 'Y', location: {site: 'oulu'}});
+      assert.equal(res.isDirty(), true);
+    });
+    it('apis', () => {
+      const resourceJson = {_id: '123', name: 'X', __v: 0};
+      const res = new Resource(transport, resourceJson);
+      res.name('Y')
+        .type('hw')
+        .location.site('oulu')
+        .location.country('finland')
+        .location.city('oulu')
+        .location.postcode('1234')
+        .location.address('street')
+        .location.room('a')
+        .location.subRoom('b')
+        .location.geo(123)
+        .hw.sn('12')
+        .hw.imei('34')
+        .hw.id('123')
+        .hw.firmware.name('q')
+        .hw.firmware.version('1');
+      const changes = {
+        name: 'Y',
+        type: 'hw',
+        location: {
+          site: 'oulu',
+          country: 'finland',
+          city: 'oulu',
+          postcode: '1234',
+          address: 'street',
+          room: 'a',
+          subRoom: 'b',
+          geo: 123
+        },
+        hw: {
+          sn: '12', imei: '34',
+          id: '123', firmware: {name: 'q', version: '1'}
+        }
+      };
+      assert.deepEqual(res.getChanges(), changes);
+      assert.equal(res.isDirty(), true);
+    });
   });
-  it('possible to change multiple', function () {
-    const resourceJson = {_id: '123', name: 'X', __v: 0};
-    const res = new Resource(transport, resourceJson);
-    assert.equal(res.name('Y').location.site('oulu').name(), 'Y');
-    assert.deepEqual(res.getChanges(), {name: 'Y', location: {site: 'oulu'}});
-    assert.equal(res.isDirty(), true);
-  });
-  describe('find', function () {
-    it('base', function () {
+  describe('find', () => {
+    it('base', () => {
       moxios.stubRequest('/api/v0/resources?', {
         status: 200,
         response: []
@@ -73,7 +114,7 @@ describe('Resource', function () {
       const find = resources.find();
       return find.exec();
     });
-    it('apis', function () {
+    it('apis', () => {
       const resources = new Resources(transport);
       const find = resources.find()
         .name('abc')
