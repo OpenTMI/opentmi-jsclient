9de2d12d3c910cbc0b68b118f35df56409aa6152
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-10-23 12:52:08 +0300

    Update mongooseQueryClient.js

--- src/utils/rest/mongooseQueryClient.js
+++ src/utils/rest/mongooseQueryClient.js
@@ -33,8 +33,7 @@ class MongooseQueryClient {
     invariant(_.isString(str), 'str should be string');
     this._query = querystring.parse(str);
     if (_.has(this._query, 'q')) {
-      const str = this._query.q;
-      this._query.q = JSON.parse(str);
+      this._query.q = JSON.parse(this._query.q);
     }
     return this;
   }
