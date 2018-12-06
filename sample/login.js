2c452c8fe932937a56482ad610edc357da89ab85
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-12-06 09:09:47 +0200

    update deps and improve sample login (#69)

--- sample/login.js
+++ sample/login.js
@@ -2,10 +2,19 @@ const {Authentication, Transport} = require('../src');
 
 const transport = new Transport('http://localhost:3000');
 const auth = new Authentication(transport);
-auth.loginWithToken(process.env.GITHUB_ACCESS_TOKEN)
+const token = process.env.GITHUB_ACCESS_TOKEN;
+const email = process.argv[1];
+const password = process.argv[2];
+let login;
+if (token) {
+  login = auth.loginWithToken(token);
+} else {
+  login = auth.login(email, password);
+}
+login
   .then(() => {
-    console.log('login success');
+    console.log(`login success, token: ${transport.token}`);
   })
   .catch((error) => {
     console.error(`login fails: ${error}`);
-  })
+  });
